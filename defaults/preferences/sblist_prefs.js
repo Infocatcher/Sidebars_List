4b9213b14739e3efcdde7935b9fbbf8a1d03e5f4
Infocatcher <infocatcher.dev.null@gmail.com>
2014-03-03 00:00:34 +0400

    Enable "openTabInSidebarClosesTab.useMove" by default (#2)

--- defaults/preferences/sblist_prefs.js
+++ defaults/preferences/sblist_prefs.js
@@ -17,7 +17,7 @@ pref("extensions.sidebarslist.removeWidthLimits", true);
 pref("extensions.sidebarslist.fixSidebarZoom", true);
 pref("extensions.sidebarslist.closeSidebarsMenu", true);
 pref("extensions.sidebarslist.openTabInSidebarClosesTab", false);
-pref("extensions.sidebarslist.openTabInSidebarClosesTab.useMove", false);
+pref("extensions.sidebarslist.openTabInSidebarClosesTab.useMove", true); // Preserve page state
 pref("extensions.sidebarslist.openTabInSidebarClosesTab.rightClickToInvert", false);
 pref("extensions.sidebarslist.reloadButtonStyle", "auto");
 // See styles for [sidebarslist_style="..."] in chrome://sidebarslist/content/sidebarslist.css
